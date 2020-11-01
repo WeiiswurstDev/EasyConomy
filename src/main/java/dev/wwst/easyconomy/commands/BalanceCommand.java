26e840a85e8cb526b4e9341d62d2eca269d3fb1b
Fruxz <cedricspitzer@outlook.de>
2020-11-01 01:37:46 +0100

    BalanceCommand >> Improved sender check

--- src/main/java/dev/wwst/easyconomy/commands/BalanceCommand.java
+++ src/main/java/dev/wwst/easyconomy/commands/BalanceCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.Objects;
@@ -29,7 +30,7 @@ public class BalanceCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if(!(sender instanceof Player)) {
+        if(sender instanceof ConsoleCommandSender) {
 
             if(args.length != 1) {
 

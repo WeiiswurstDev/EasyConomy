587f27a55c487a427283991e184e69cd5eda148a
WeiiswurstDev <arved.ehrnsperger@gmail.com>
2020-11-18 19:19:41 +0100

    Added French translation - French translation: Sonyk#4826 <3 - Added credits to the top of each messages file
    Signed-off-by: WeiiswurstDev <arved.ehrnsperger@gmail.com>

--- src/main/java/dev/wwst/easyconomy/utils/MessageTranslator.java
+++ src/main/java/dev/wwst/easyconomy/utils/MessageTranslator.java
@@ -16,7 +16,8 @@ public class MessageTranslator {
 
     private static final String[] translations = new String[] {
             "de",
-            "en"
+            "en",
+            "fr",
     };
 
     private static MessageTranslator INSTANCE;
